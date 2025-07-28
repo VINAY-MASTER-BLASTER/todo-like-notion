7c692ef04eeee496e83610465f99b01d7f829a98
VINAY-MASTER-BLASTER <vinaytoppo145@gmail.com>
2025-07-28 17:52:21 +0530

    todo

--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -6,17 +6,20 @@ const TaskForm = () => {
         <form>
             <input type="text" className='task_input' placeholder='Enter Your Task'/>
             <div className='task_form_bottom_line'>
+              <div>
                 <button className='tag'>HTML</button>
                 <button className='tag'>CSS</button>
                 <button className='tag'>JavaScript</button>
                 <button className='tag'>React</button>
-
+                </div>
+                <div>
                 <select className='task_status'>
                     <option value="todo">To do</option>
                     <option value="doing">Doing</option>
                     <option value="done">Done</option>
                 </select>
-                <button type='sunbit' className='task_sumbit'>+ Add </button>
+                <button type='sunbit' className='task_sumbit'>+ Add Task</button>
+                </div>
             </div>
         </form>
     </header>
