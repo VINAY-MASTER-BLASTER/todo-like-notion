46e4705f77355479ec2f78a5b949d145fda039cb
VINAY-MASTER-BLASTER <vinaytoppo145@gmail.com>
2025-07-30 15:40:33 +0530

    todo

--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -1,11 +1,38 @@
-import {React} from 'react'
-import "../Components/TaskFrom.css"
+import {useState} from 'react'
+import '../Components/TaskFrom.css'
 import Tag from './Tag'
 const TaskForm = () => {
+  const [Taskdata,Settaskdata] = useState({
+    Task : "",
+    Status: "todo"
+  })
+  const handleChange = (e) =>{
+    const {name,value} = e.target
+    Settaskdata((prev) => {
+      return {...prev, [name] :value}
+    })
+  }
+  const handlesumit=(e)=>{
+    e.preventDefault();
+    console.log(Taskdata)
+  }
+
+
+  // const [task,settask] = useState("")
+  // const [status,setstauste] = useState("ToDo")
+
+  // const handleTaskChange = (e) =>{
+  //   settask(e.target.value)
+  // }
+  // const handlestatuschange = (e) =>{
+  //   setstauste(e.target.value)
+  // }
+  // console.log(task,status)
+
   return (
     <header className='app_header'>
-        <form>
-            <input type="text" className='task_input' placeholder='Enter Your Task'/>
+        <form onSubmit={handlesumit}>
+            <input type="text" className='task_input' name="Task" placeholder='Enter Your Task' onChange={handleChange}/>
             <div className='task_form_bottom_line'>
               <div>
                 <Tag tagName="HTML"/>
@@ -14,7 +41,7 @@ const TaskForm = () => {
                 <Tag tagName="React"/>
                 </div>
                 <div>
-                <select className='task_status'>
+                <select name="Status" className='task_status' onChange={handleChange}>
                     <option value="todo">To do</option>
                     <option value="doing">Doing</option>
                     <option value="done">Done</option>
